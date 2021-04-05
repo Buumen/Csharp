17705a4c944665625560f318ed49ce31df8b7874
Csaba Sipos <buumen79@gmail.com>
2021-04-05 21:44:37 +0200

    v1.0

---
+++ beadando_lazy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace beadando_lazy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchText = "";
+            string searchInText = "";
+            Lazy<RegexClass> lazyRegexObject = new Lazy<RegexClass>(() => new RegexClass(searchText, searchInText));
+
+            Console.WriteLine("IsValueCreated = {0}", lazyRegexObject.IsValueCreated);
+
+            Console.WriteLine("Adjon meg egy tetszőleges szöveget:\n");
+             searchInText = Console.ReadLine();
+            Console.WriteLine("Keresendő szöveg:\n");
+             searchText = Console.ReadLine();
+
+
+            lazyRegexObject.Value.keres();
+            Console.WriteLine("IsValueCreated = {0}", lazyRegexObject.IsValueCreated);
+
+           
+            
+
+
+
+
+        }
+    }
+}
---
+++ beadando_lazy/RegexClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace beadando_lazy
+{
+    class RegexClass
+    {
+        public string SearchString { get; set; }
+        public string SearchIn { get; set; }
+
+        public RegexClass(string searchString, string searchIn)
+        {
+            
+            SearchString = searchString;
+            SearchIn = searchIn;
+        }
+
+        
+       
+        public void keres()
+        {
+            
+            var minta1 = @"\b" + SearchString + "\b";
+            Regex rx = new Regex(minta1);
+
+            Console.WriteLine("Szöveg:\n {0}", SearchIn);
+            Console.WriteLine("Keresendő szöveg: {0}", SearchString);
+
+            MatchCollection matches = Regex.Matches(SearchIn, @"\b" + SearchString + @"\b");
+
+
+            Console.WriteLine("Találatok száma: {0}\n",
+                              matches.Count
+                              );
+
+            // Report on each match.
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("Pozíció:" + match.Index);
+            }
+        }
+    }
+}
